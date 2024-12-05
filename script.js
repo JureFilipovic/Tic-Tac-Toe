9c02869634d640b0dda620373fa38df17038030b
JureFilipovic <jure.filipovic@gmail.com>
2024-12-05 15:09:51 +0100

    Add Gameboard function to create, manage and print 3x3 board
    -Added logic to initialize a 3x3 board with Cell
     objects.
    -Implemented dropToken method to place tokens for
     players
    -Created printBoard method for printing a board in
     console and getBoard method for returning actual
     board (will be used for rendering)

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+/*
+** The Gameboard represents the state of the board.
+** Each square holds a Cell and we expose a dropToken method
+** to add cells to squares.
+*/
+function Gameboard () {
+    const board = [];
+
+    // Create 3x3 2d array that will represent the board
+    // and fill it with empty cells.
+    for (let i = 0; i < 3; i++) {
+        board[i] = [];
+        for (let j = 0; j < 3; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    // Method for getting the entire board for rendering.
+    const getBoard = () => board;
+
+    // Method for adding a player's token to the cell.
+    const dropToken = (row, column, playerToken) => {
+        // Checks if the cell is empty
+        if (board[row][column].getValue() !== "") return;
+        // Place the player's token in the empty cell.
+        board[row][column].addToken(playerToken);
+    };
+
+    // Method for printing a board filled with cell values.
+    const printBoard = () => {
+        const boardCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log (boardCellValues);
+    }
+    
+    return {
+        getBoard,
+        dropToken,
+        printBoard
+    }
+}
+
+
+
 
 /*
 ** A Cell represents one field on the board
@@ -5,7 +48,6 @@
 ** X: Player one's token,
 ** O: Player two's token.
 */
-
 function Cell () {
     let value = "";
 
